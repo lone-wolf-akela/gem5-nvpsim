3fa117d7a027d883dd316338f3efca2b18a77a9e
lone_wolf_akela <lone_wolf_akela@qq.com>
2018-05-20 22:12:18 +0800

    confirmed circnn working

--- gem5/configs/example/LiuRuoyang_circnn.py
+++ gem5/configs/example/LiuRuoyang_circnn.py
@@ -6,8 +6,8 @@ from m5.objects import *
 #	os.remove("m5out/devicedata")
 
 import sys  
-#cap = float(sys.argv[1])/10
-#profilemult = float(sys.argv[2])/10
+cap = 100
+profilemult = 10
 
 #ticks_per_sec = 1000000000000
 #max_sec = 100
@@ -21,8 +21,8 @@ system.mem_ranges = [AddrRange('512MB')]
 
 #vdev
 system.has_vdev = 1
-system.vdev_ranges = [AddrRange('512MB', '512MB')]#, AddrRange('513MB', '513MB'), AddrRange('514MB', '514MB')]
-system.vaddr_vdev_ranges = [AddrRange('1000MB', '1000MB')]#, AddrRange('1001MB', '1001MB'), AddrRange('1002MB', '1002MB')]
+system.vdev_ranges = [AddrRange('512MB', '513MB')]#, AddrRange('513MB', '513MB'), AddrRange('514MB', '514MB')]
+system.vaddr_vdev_ranges = [AddrRange('1000MB', '1001MB')]#, AddrRange('1001MB', '1001MB'), AddrRange('1002MB', '1002MB')]
 ###
 
 #energy mgmt
@@ -32,8 +32,8 @@ system.energy_mgmt.state_machine.thres_high = 10
 system.energy_mgmt.state_machine.thres_low = 5
 
 
-system.energy_mgmt.capacity = 100;	#uF
-system.energy_mgmt.energy_profile_mult = 1; 
+system.energy_mgmt.capacity = cap;	#uF
+system.energy_mgmt.energy_profile_mult = profilemult; 
 ###
 
 #set some parameters for the CPU
@@ -70,10 +70,11 @@ system.vdev1.is_interruptable = 0
 system.vdev1.port = system.membus.master
 system.vdev1.s_energy_port = system.energy_mgmt.m_energy_port
 
-system.vdev3.need_log = 1
+#system.vdev3.need_log = 1
 
 process = LiveProcess()
 process.cmd = ['tests/test-progs/circnn/main']
+#process.cmd = ['tests/test-progs/queens/queens']
 system.cpu.workload = process
 system.cpu.createThreads()
 
--- gem5/tests/test-progs/circnn/main.c
+++ gem5/tests/test-progs/circnn/main.c
@@ -9,43 +9,25 @@
 #include "w3.h"
 #include "b3.h"
 
-#define BATCH 10
+#define BATCH 62
 
 int main()
 {
 	int count,pic,i,j,index,correct = 0;
-	//int acc_x, acc_y, acc_z, tmp, payload_size, i, count = 0;
-	//uint8_t collection[1+2+6*ANum];
-	//uint8_t * payload;
-	volatile uint8_t * data;//, * acc_reg, * rf_reg;
-	//uint8_t ImagIn[16]={0};
-	//uint8_t ImagOut[16];
-	//uint8_t RealOut[16];
+	volatile uint8_t * data;
 	double layer3[8][64];
 	uint8_t finalresult[BATCH*8];
 	printf("Program Start.\n");
 
-	// peripheral register
-	//periRegister(TMP_SENSOR_ID, tmp_reg);
-	//periRegister(ACC_SENSOR_ID, acc_reg);
-	//periRegister(RF_ID, rf_reg);
-
-	//tmp_reg = (uint8_t*) mmap(PERI_ADDR[TMP_SENSOR_ID], sizeof(uint8_t), PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
-	//acc_reg = (uint8_t*) mmap(PERI_ADDR[ACC_SENSOR_ID], sizeof(uint8_t), PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
-	//rf_reg = (uint8_t*) mmap(PERI_ADDR[RF_ID], sizeof(uint8_t), PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
 	data = (uint8_t*) mmap(PERI_ADDR[0], sizeof(double)*64*8+1, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
 	printf("Peripherals Registered.\n");
 
 	for(count = 0; count < BATCH; count++)
 	{
-
-		// initialization 
 		periInit(data);         
 		printf("Inited.\n");
 
-		// data read 
 		periRead(data, &(layer3[0][0]), sizeof(double)*64*8);
-		//computation
 		for (pic = 0; pic < 8; pic++)
 		{
 			double result[10];
@@ -70,11 +52,8 @@ int main()
 
 
 		printf("Complete the %d-th packet.\n", count);
-		
-		//DPRINTF(EXECUTION, "Complete the %d-th packet.\n", ++count);
 
 	}
-	// peripheral logout
 	periLogout(0);
 	for(i = 0;i<BATCH*8;i++)
 	{
